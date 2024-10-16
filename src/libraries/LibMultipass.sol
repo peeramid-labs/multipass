6193587f70d95c75102e3c3ef4838ce5c8b6f4db
Tim Pechersky <tim.pechersky@gmail.com>
2024-10-16 11:08:58 +0700

    change slot name,

--- src/libraries/LibMultipass.sol
+++ src/libraries/LibMultipass.sol
@@ -68,7 +68,7 @@ library LibMultipass {
         uint256 validUntil;
     }
 
-    bytes32 constant MULTIPASS_STORAGE_POSITION = keccak256("multipass.diamond.storage.position");
+    bytes32 constant private MULTIPASS_STORAGE_POSITION = bytes32(uint256(keccak256('multipass.storage.struct')) - 1);
 
     /**
      * @dev The domain name of the registrar.
